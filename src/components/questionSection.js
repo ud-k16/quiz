4bd7addeccd62005ee24d7ad1a631fd18635f97e
uma <forjust224@gmail.com>
2023-02-05 20:17:53 +0530

    button disabled if not choice is chosen

--- src/components/questionSection.js
+++ src/components/questionSection.js
@@ -1,14 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { Button, Radio, FormControl, FormControlLabel } from '@mui/material';
 import { displayQuestion, updateAnswer, userChoice } from '../store/reducer';
+import { useState } from 'react';
 
 const QuestionDisplay = ({ setShowResult }) => {
-  const { displayQuestionNumber, questions, totalQuestions } = useSelector(
-    (state) => state
-  );
+  const { displayQuestionNumber, questions, totalQuestions, userAnswer } =
+    useSelector((state) => state);
   const dispatch = useDispatch();
+  const [selected, setSelected] = useState(false);
 
-  const AnswerOptions = () => {
+  const AnswerOptions = ({ setSelected }) => {
     return (
       <FormControl>
         {questions[displayQuestionNumber].choices.map((option, index) => {
@@ -19,7 +20,10 @@ const QuestionDisplay = ({ setShowResult }) => {
               label={option}
               labelPlacement="end"
               key={index}
-              onClick={(event) => dispatch(userChoice(event.target.value))}
+              onClick={(event) => {
+                dispatch(userChoice(event.target.value));
+                setSelected(true);
+              }}
               // defaultChecked={true}
             />
           );
@@ -31,17 +35,19 @@ const QuestionDisplay = ({ setShowResult }) => {
   return (
     <div className="questionSection">
       <div>{questions[displayQuestionNumber].question}</div>
-      <AnswerOptions />
+      <AnswerOptions setSelected={setSelected} />
       <Button
         variant="contained"
         onClick={() => {
-          console.log('presses');
           dispatch(updateAnswer(displayQuestionNumber));
+
           if (displayQuestionNumber != totalQuestions - 1)
             dispatch(displayQuestion(displayQuestionNumber + 1));
           else setShowResult(true);
+          setSelected(false);
         }}
         style={{ width: '20%' }}
+        disabled={!selected}
       >
         save And Continue
       </Button>
